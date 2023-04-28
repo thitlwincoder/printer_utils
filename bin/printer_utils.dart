bdf9edd23fe0c1fa7f5d329c599109889c936b65
thitlwincoder <thitlwincoder@gmail.com>
2023-04-28 20:06:36 +0630

    first commit

---
+++ bin/printer_utils.dart
@@ -0,0 +1,5 @@
+import 'package:printer_utils/printer_utils.dart' as printer_utils;
+
+void main(List<String> arguments) {
+  print('Hello world: ${printer_utils.calculate()}!');
+}
---
+++ test/printer_utils_test.dart
@@ -0,0 +1,8 @@
+import 'package:printer_utils/printer_utils.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
